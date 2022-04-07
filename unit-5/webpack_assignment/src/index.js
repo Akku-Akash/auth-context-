3c034f285a649bcca6967fef44737f0d7c3e1a36
Akashhhhhh <96081139+Akashhhhhh@users.noreply.github.com>
2022-04-07 16:55:37 +0530

    abc

--- unit-5/webpack_assignment/src/index.js
+++ unit-5/webpack_assignment/src/index.js
@@ -4,20 +4,21 @@ import logo from "./img.png"
 
 const img = document.createElement("img")
 img.src = logo;
-
+img.style.width = "200px"
 const h1 = document.createElement("h1")
 h1.innerText = "TODO List"
 //h1.classList.add("todo")
 
 const input = document.createElement("input")
-input.classList.add("inp")
+// input.classList.add("inp")
+
 
 const button = document.createElement("button")
 button.classList.add("btn")
 button.innerText = "Button"
 button.onclick = ()=>{
     let el = input.value
-    let h4 = document.createElement("h4")
+    let h4 = document.createElement("ul")
     h4.innerText = el;
     document.getElementById("list").appendChild(h4)
 }
