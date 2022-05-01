00d8ab7acf6ab9de4702f018007e95caf4ef1972
Akashhhhhh <96081139+Akashhhhhh@users.noreply.github.com>
2022-05-01 15:01:47 +0530

    aba

--- unit-5b/auth_context/auth-context-/src/components/Status.jsx
+++ unit-5b/auth_context/auth-context-/src/components/Status.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
 
 export const Status = () =>{
-
+//
     const {user} = useContext(AuthContext);
     return(
         <h3>{user? `User is logged in with token ${user}`:"User is not loged in"}</h3>
